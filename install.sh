62ea52e9c9dc19cd2b2a2ce914944ca0334a648d
joe1014 <joe.tialtngo@gmail.com>
2014-02-12 02:56:17 +0900

    install.sh modify

--- install.sh
+++ install.sh
@@ -12,11 +12,11 @@ do
   test $file == "." && continue
   test $file == ".." && continue
  
-  if [ ! -e "${homefile}" ]; then
-     echo "${file} not exis, make symbolic link to ${homefile}"
-  else
+  if [ -e "${homefile}" ]; then
      echo "${file} exist. then rm ${homefile} and make sysmbolic link from ${filepath} "
      rm "${homefile}"
+  else
+     echo "${file} not exis, make symbolic link to ${homefile}"
   fi
   ln -s "${filepath}" "${homefile}"
 done
