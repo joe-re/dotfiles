72c9c8e721e9ec17529387ab6fcb3dfbde983a2f
joe-re <joe.tialtngo@gmail.com>
2014-11-11 01:26:01 +0900

    symlinkを作って、karabinerのprivate.xmlを作って、vimのbackupdirを作る

--- install.sh
+++ install.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
- 
+set -e
+# brew bundle 叩く
+brew bundle || true
+
+# 設定ファイルのシンボリックを作成
+echo "start to make sym link"
 files=("gitconfig" "vimrc" "zshrc" "ctags" "tmux.conf" "atom" "zprofile" "vrapperrc" "rubocop.yml", "oh-my-zsh")
- 
+
+# to home dir
 for file in ${files[*]}
 do
-   filepath="${PWD}/${file}"
-   homefile="${HOME}/.${file}"
- 
-  if [ -e "${homefile}" ]; then
-     echo "${file} exist. then rm ${homefile} and make sysmbolic link from ${filepath} "
-     rm "${homefile}"
-  else
-     echo "${file} not exis, make symbolic link to ${homefile}"
-  fi
-  ln -s "${filepath}" "${homefile}"
+  filepath="${PWD}/${file}"
+  homefile="${HOME}/.${file}"
+  ln -sf "${filepath}" "${homefile}"
+  echo "made symlink: ${homefile}"
 done
+
+# karabiner
+karabiner_file="${PWD}/private.xml"
+to_karabiner_file=~/Library/Application\ Support/Karabiner/private.xml
+ln -sf "${karabiner_file}" "${to_karabiner_file}"
+echo "made symlink: ${to_karabiner_file}"
+
+# mkdir vim backup dir
+vim_backup_dir="${HOME}/.vim-backup"
+if test -e "${vim_backup_dir}"; then
+  echo "${vim_backup_dir} is exists"
+else
+  mkdir "${vim_backup_dir}"
+  echo "made dir: ${vim_backup_dir}"
+fi
+
+echo "completed!"
